aa7433d96ea0f18d6b8c561c5bfffb9a4c66f5bf
Conrad Bohm <conrad.bohm@hotmail.co.uk>
2025-01-16 13:19:02 +0000

    name correctly

--- pipes.tf
+++ pipes.tf
@@ -1,12 +1,12 @@
 resource "aws_pipes_pipe" "basket" {
-  name     = "basket-pipe"
+  name     = "silver-bullet-basket-pipe"
   role_arn = "arn:aws:iam::536697261635:user/silver-bullet"
   source   = "arn:aws:sns:eu-west-1:536697261635:silver-bullet-basket-events"
   target   = "arn:aws:events:eu-west-1:536697261635:event-bus/silver-bullet-domain-events"
 }
 
 resource "aws_pipes_pipe" "checkout" {
-  name     = "checkouts-pipe"
+  name     = "silver-bullet-checkouts-pipe"
   role_arn = "arn:aws:iam::536697261635:user/silver-bullet"
   source   = "arn:aws:sns:eu-west-1:536697261635:silver-bullet-checkouts-events"
   target   = "arn:aws:events:eu-west-1:536697261635:event-bus/silver-bullet-domain-events"
