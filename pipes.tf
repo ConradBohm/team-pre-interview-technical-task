70f965a86076f0b151e379b33bc2c60c4bc88e11
Conrad Bohm <conrad.bohm@hotmail.co.uk>
2025-01-16 16:44:44 +0000

    reorganise

--- pipes.tf
+++ pipes.tf
@@ -1,17 +1,3 @@
-resource "aws_sqs_queue" "basket" {
-  name                      = "silver-bullet-basket-queue"
-  delay_seconds             = 90
-  max_message_size          = 2048
-  message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
-}
-
-resource "aws_sns_topic_subscription" "basket_sqs_target" {
-  topic_arn = "arn:aws:sns:eu-west-1:536697261635:silver-bullet-basket-events"
-  protocol  = "sqs"
-  endpoint  = aws_sqs_queue.basket.arn
-}
-
 resource "aws_pipes_pipe" "basket" {
   name     = "silver-bullet-basket-pipe"
   role_arn = aws_iam_role.pipes.arn
@@ -23,20 +9,6 @@ resource "aws_pipes_pipe" "basket" {
       { "event": <$.body> }
     EOT
   }
-}
-
-resource "aws_sqs_queue" "checkout" {
-  name                      = "silver-bullet-checkout-queue"
-  delay_seconds             = 90
-  max_message_size          = 2048
-  message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
-}
-
-resource "aws_sns_topic_subscription" "checkout_sqs_target" {
-  topic_arn = "arn:aws:sns:eu-west-1:536697261635:silver-bullet-checkout-events"
-  protocol  = "sqs"
-  endpoint  = aws_sqs_queue.checkout.arn
 }
 
 resource "aws_pipes_pipe" "checkout" {
---
+++ sqs.tf
@@ -0,0 +1,31 @@
+resource "aws_sqs_queue" "basket" {
+  name                      = "silver-bullet-basket-queue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+}
+
+resource "aws_sns_topic_subscription" "basket_sqs_target" {
+  topic_arn = "arn:aws:sns:eu-west-1:536697261635:silver-bullet-basket-events"
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.basket.arn
+}
+
+# =============================================================================================
+
+resource "aws_sqs_queue" "checkout" {
+  name                      = "silver-bullet-checkout-queue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+}
+
+resource "aws_sns_topic_subscription" "checkout_sqs_target" {
+  topic_arn = "arn:aws:sns:eu-west-1:536697261635:silver-bullet-checkout-events"
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.checkout.arn
+}
+
+# =============================================================================================
