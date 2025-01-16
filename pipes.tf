8cca9a0b128017a3aa95fd21b1a161a8aabd0916
Conrad Bohm <conrad.bohm@hotmail.co.uk>
2025-01-16 13:06:29 +0000

    apply pipes

---
+++ pipes.tf
@@ -0,0 +1,13 @@
+resource "aws_pipes_pipe" "example" {
+  name     = "example-pipe"
+  role_arn = "arn:aws:iam::536697261635:user/silver-bullet"
+  source   = "arn:aws:sns:eu-west-1:536697261635:silver-bullet-basket-events"
+  target   = "arn:aws:events:eu-west-1:536697261635:event-bus/silver-bullet-domain-events"
+}
+
+resource "aws_pipes_pipe" "example" {
+  name     = "example-pipe"
+  role_arn = "arn:aws:iam::536697261635:user/silver-bullet"
+  source   = "arn:aws:sns:eu-west-1:536697261635:silver-bullet-checkouts-events"
+  target   = "arn:aws:events:eu-west-1:536697261635:event-bus/silver-bullet-domain-events"
+}
