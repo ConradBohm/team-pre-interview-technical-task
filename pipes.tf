925a3205e89e95287d0eed04db59dcc985265d93
Conrad Bohm <conrad.bohm@hotmail.co.uk>
2025-01-16 15:44:38 +0000

    no redrive policy

--- pipes.tf
+++ pipes.tf
@@ -4,10 +4,6 @@ resource "aws_sqs_queue" "basket" {
   max_message_size          = 2048
   message_retention_seconds = 86400
   receive_wait_time_seconds = 10
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.terraform_queue_deadletter.arn
-    maxReceiveCount     = 4
-  })
 }
 
 resource "aws_sns_topic_subscription" "basket_sqs_target" {
@@ -29,10 +25,6 @@ resource "aws_sqs_queue" "checkouts" {
   max_message_size          = 2048
   message_retention_seconds = 86400
   receive_wait_time_seconds = 10
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.terraform_queue_deadletter.arn
-    maxReceiveCount     = 4
-  })
 }
 
 resource "aws_sns_topic_subscription" "checkouts_sqs_target" {
