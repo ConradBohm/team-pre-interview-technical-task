20cb6b4b13c2cc7a57905ffd626b63c98f9792dd
Conrad Bohm <conrad.bohm@hotmail.co.uk>
2025-01-16 16:28:51 +0000

    move iam role

---
+++ iam.tf
@@ -0,0 +1,46 @@
+resource "aws_iam_role" "pipes" {
+  name = "silver-bullet-pipes-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "pipes.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "pipes" {
+  role = aws_iam_role.pipes.id
+  name = "Pipes"
+  policy = jsonencode({
+    Statement = [{
+      Effect = "Allow"
+      Action = ["pipes:*"]
+      Resource = ["arn:aws:pipes:eu-west-1:536697261635:pipes/silver-bullet-*"]
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "sqs" {
+  role = aws_iam_role.pipes.id
+  name = "SQS"
+  policy = jsonencode({
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+            "sqs:ReceiveMessage",
+            "sqs:DeleteMessage",
+            "sqs:GetQueueAttributes"
+        ]
+      Resource = "arn:aws:sqs:eu-west-1:536697261635:silver-bullet-*"
+    }]
+  })
+}
+
+# =============================================================================================
