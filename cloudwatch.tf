8d307d90f1d06569e17f16a2d6fb3e53d64271b3
Conrad Bohm <conrad.bohm@hotmail.co.uk>
2025-01-17 11:37:56 +0000

    use correct arn

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -10,7 +10,7 @@ resource "aws_cloudwatch_event_rule" "console" {
 resource "aws_cloudwatch_event_target" "purchase" {
   rule           = aws_cloudwatch_event_rule.console.name
   target_id      = "silver-bullet-send-to-purchase"
-  arn            = aws_sns_topic.aws_logins.arn
-  sqs_target     = ws_sqs_queue.purchase.arn
+  arn            = aws_sqs_queue.purchase.arn
+  #sqs_target     = aws_sqs_queue.purchase.arn
   event_bus_name = "arn:aws:events:eu-west-1:536697261635:event-bus/silver-bullet-domain-events"
 }
