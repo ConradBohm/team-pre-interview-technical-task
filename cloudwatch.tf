32915f63536f3f30a418261d8e6a8568852ac62b
Conrad Bohm <conrad.bohm@hotmail.co.uk>
2025-01-17 11:55:20 +0000

    rename

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,4 +1,4 @@
-resource "aws_cloudwatch_event_rule" "console" {
+resource "aws_cloudwatch_event_rule" "purchase" {
   name        = "silver-bullet-capture-basket-checkout"
   description = "Capture each event from Basket and Checkout topics"
 
@@ -8,7 +8,7 @@ resource "aws_cloudwatch_event_rule" "console" {
 }
 
 resource "aws_cloudwatch_event_target" "purchase" {
-  rule           = aws_cloudwatch_event_rule.console.name
+  rule           = aws_cloudwatch_event_rule.purchase.name
   target_id      = "silver-bullet-send-to-purchase"
   arn            = aws_sqs_queue.purchase.arn
   #sqs_target     = aws_sqs_queue.purchase.arn
