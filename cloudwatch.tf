0c6c927ea5fc747a814a17965ae65a769859c4e9
Conrad Bohm <conrad.bohm@hotmail.co.uk>
2025-01-17 13:01:39 +0000

    cloudwatch logs

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -14,3 +14,15 @@ resource "aws_cloudwatch_event_target" "purchase" {
   arn            = aws_sqs_queue.purchase.arn
   event_bus_name = "arn:aws:events:eu-west-1:536697261635:event-bus/silver-bullet-domain-events"
 }
+
+# =============================================================================================
+
+resource "aws_cloudwatch_log_group" "silver" {
+  name              = "silver-bullet-log-group"
+  retention_in_days = 5
+}
+
+resource "aws_cloudwatch_log_stream" "silver" {
+  name           = "silver-bullet-log-stream"
+  log_group_name = aws_cloudwatch_log_group.silver.name
+}
