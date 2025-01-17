ff33b13b412c1f10cbce841f121c533bcbce1e62
Conrad Bohm <conrad.bohm@hotmail.co.uk>
2025-01-17 12:02:39 +0000

    event target

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -7,10 +7,10 @@ resource "aws_cloudwatch_event_rule" "purchase" {
   })
 }
 
-# resource "aws_cloudwatch_event_target" "purchase" {
-#   rule           = aws_cloudwatch_event_rule.purchase.name
-#   target_id      = "silver-bullet-send-to-purchase"
-#   arn            = aws_sqs_queue.purchase.arn
-#   #sqs_target     = aws_sqs_queue.purchase.arn
-#   event_bus_name = "arn:aws:events:eu-west-1:536697261635:event-bus/silver-bullet-domain-events"
-# }
+resource "aws_cloudwatch_event_target" "purchase" {
+  rule           = aws_cloudwatch_event_rule.purchase.name
+  target_id      = "silver-bullet-send-to-purchase"
+  arn            = aws_sqs_queue.purchase.arn
+  #sqs_target     = aws_sqs_queue.purchase.arn
+  event_bus_name = "arn:aws:events:eu-west-1:536697261635:event-bus/silver-bullet-domain-events"
+}
