6df136c4e6dc3cfef47b1ac8ea398b7773555dde
Conrad Bohm <conrad.bohm@hotmail.co.uk>
2025-01-17 12:09:06 +0000

    no sqs info needed

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -12,6 +12,5 @@ resource "aws_cloudwatch_event_target" "purchase" {
   rule           = aws_cloudwatch_event_rule.purchase.name
   target_id      = "silver-bullet-send-to-purchase"
   arn            = aws_sqs_queue.purchase.arn
-  #sqs_target     = aws_sqs_queue.purchase.arn
   event_bus_name = "arn:aws:events:eu-west-1:536697261635:event-bus/silver-bullet-domain-events"
 }
